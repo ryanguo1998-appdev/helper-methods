336b97c33158b49b58c3e8a640b3e26c20ea060a
ryanguo1998 <rguo98@uchicago.edu>
2022-04-07 17:47:55 +0000

    controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create" as: :movies
-  get "/movies/new" => "movies#new" as: :new_movie
+  post "/movies" => "movies#create", as: :movies
+  get "/movies/new" => "movies#new", as: :new_movie
           
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show" as: :movie
+  get "/movies/:id" => "movies#show", as: :movie
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit" as: :edit_movie
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
